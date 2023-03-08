411b82774c88c4323b24c665e0abf15730777efc
glikched <florianvictorcristea@gmail.com>
2023-03-08 19:29:30 +0200

    crowdin: Changes for DerpFest

--- .github/scripts/push-crowdin.sh
+++ .github/scripts/push-crowdin.sh
@@ -1,9 +1,9 @@
           mkdir pixys
           cd pixys
-          repo init -u https://github.com/PixysOS/manifest -b thirteen -m crowdin.xml --depth=1
+          repo init -u https://github.com/derpfest-aosp/manifest -b 13 -m crowdin.xml --depth=1
           repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
           wait
           cd crowdin
-          export CROWDIN_BASE_PATH=/home/runner/work/crowdin/crowdin/pixys
+          export CROWDIN_BASE_PATH=/home/runner/work/crowdin/crowdin/derpfest
           CROWDIN_API_KEY=$CROWDIN_API_KEY
-          ./crowdin_sync.py --username Darkstar085 --branch thirteen --upload-sources --upload-translations
+          ./crowdin_sync.py --username victor4cris --branch 13 --upload-sources --upload-translations
--- .github/scripts/setup-crowdin.sh
+++ .github/scripts/setup-crowdin.sh
@@ -5,7 +5,7 @@
           wget https://artifacts.crowdin.com/repo/deb/crowdin3.deb -O crowdin.deb
           sudo dpkg -i crowdin.deb
           wait
-          git clone https://github.com/PixysOS/crowdin.git crowdin
+          git clone https://github.com/derpfest-aosp/crowdin.git crowdin
           cd crowdin
           pip install -r requirements.txt
           wait
